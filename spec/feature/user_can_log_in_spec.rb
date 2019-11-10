84bfe8d861e39f8bc116b26e7f755783165f25d9
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-10 12:35:33 -0300

    User can LogOut

--- spec/feature/user_can_log_in_spec.rb
+++ spec/feature/user_can_log_in_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'User can login' do
   scenario 'Sucessfully'do
-    user = User.create!(email: 'user@email.com', password: '123456')
+    user = create(:user)
 
     visit root_path
     click_on 'Log In'
@@ -26,6 +26,23 @@ feature 'User can login' do
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(current_path).to eq root_path
+  end
+
+  scenario 'and can log out' do
+    user = create(:user) 
+
+    visit root_path
+    click_on 'Log In'
+    within('form#new_user') do
+      fill_in 'Email:', with: user.email
+      fill_in 'Password:', with: '123456'
+      click_on 'Log In!'
+    end
+    click_on 'Log Out'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content('Signed out successfully') 
+    expect(page).to have_content('Log In')
   end
 
 end
