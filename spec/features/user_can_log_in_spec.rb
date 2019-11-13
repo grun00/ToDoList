e4459fb9481f4dc64ab2938e7c8262723513dcfe
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-12 23:23:48 -0300

    Corrected Login Tests

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -7,9 +7,9 @@ feature 'User can login' do
     visit root_path
     click_on 'Log In'
     within('form#new_user') do
-      fill_in 'Email:', with: user.email
-      fill_in 'Password:', with: '123456'
-      click_on 'Log In!'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: '123456'
+      click_on 'Log in'
     end
 
     expect(page).to have_content('Signed in successfully.') 
@@ -17,12 +17,11 @@ feature 'User can login' do
 
   scenario 'and can create an Account' do
     visit root_path
-    click_on 'Log In'
+    click_on 'Create Account'
+    fill_in 'Email', with: 'user@email.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
     click_on 'Sign up'
-    fill_in 'Email:', with: 'user@email.com'
-    fill_in 'Password:', with: '123456'
-    fill_in 'Password Confirmation:', with: '123456'
-    click_on 'Sign up!'
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(current_path).to eq root_path
---
+++ spec/features/user_can_search_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+feature 'User can search for tasks' do
+end
