9e1f0f147a7e5f913ca344e535de22221a0c5d5e
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-15 16:38:16 -0300

    Fixed Seeds for profile creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
+path = Rails.root.join 'app', 'assets', 'images', 'test-image.png'
 user = User.create(email: 'a@b.c', password: '123456')
-Profile.create(nickname: 'KillerQueen', bio: 'Just your normal User', user: user, avatar: File.new('/spec/support/assets/test-image.png', 'rb'))
+Profile.create(nickname: 'KillerQueen', bio: 'Just your normal User', user: user).avatar.attach(io: File.open(path), filename: "test-image.png")
+
+
 adm = User.create(email: 'adm@email.com', password: '123456')
-Profile.create(nickname: 'RadioGaga', bio: 'Just your administration User', user: adm, avatar: File.new('/spec/support/assets/test-image.png', 'rb'))
+Profile.create(nickname: 'RadioGaga', bio: 'Just your administration User', user: adm).avatar.attach(io: File.open(path), filename: "test-image.png")
+
 
 for i in 0..2 do
   task = Task.create(title: "Example Task \##{i + 1}", description: 'A Task can have a description of up to 280 characters!', priority: (10*i), user: adm, status: 10, share: true)
