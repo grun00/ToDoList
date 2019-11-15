27c9868584a07e2d415153d953f73dcf8d4ff020
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-15 16:48:36 -0300

    Tests for atributes validation

--- spec/features/profile_creation_spec.rb
+++ spec/features/profile_creation_spec.rb
@@ -36,19 +36,80 @@ feature 'User creates a profile' do
   end
 
   scenario 'And Must have a Unique Nickname' do
+    user = create(:user)
+    other_user = create(:user)
+    create(:profile, user: other_user, nickname: 'JoJoFan')
+    login_as(user)
 
+    visit root_path
+
+    fill_in 'Nickname:', with: 'JoJoFan'
+    fill_in 'Bio', with: 'A Personal Bio'
+    attach_file('Your Profile Picture', 'spec/support/assets/test-image.png')
+    click_on 'Create Profile'
+
+    expect(page).to have_content('Nickname Taken! Chose Another one')
+    expect(current_path).to eq new_profile_path 
   end
 
   scenario 'And Must have at least 5 characters' do
+    user = create(:user)
+    login_as(user)
 
+    visit root_path
+
+    fill_in 'Nickname:', with: ''
+    fill_in 'Bio', with: 'A Personal Bio'
+    attach_file('Your Profile Picture', 'spec/support/assets/test-image.png')
+    click_on 'Create Profile'
+
+    expect(page).to have_content('Nickname too short, Minimum of 4 characters')
+    expect(current_path).to eq new_profile_path 
   end
 
   scenario 'And bio Cannot be empty' do
+    user = create(:user)
+    login_as(user)
 
+    visit root_path
+
+    fill_in 'Nickname:', with: 'Test123'
+    fill_in 'Bio', with: ''
+    attach_file('Your Profile Picture', 'spec/support/assets/test-image.png')
+    click_on 'Create Profile'
+
+    expect(page).to have_content('Bio cannot be blank')
+    expect(current_path).to eq new_profile_path 
   end
+
+  scenario 'And bio Cannot have more than 500 characters' do
+    user = create(:user)
+    login_as(user)
+
+    visit root_path
+
+    fill_in 'Nickname:', with: 'Test123'
+    fill_in 'Bio', with: "A" * 501
+    attach_file('Your Profile Picture', 'spec/support/assets/test-image.png')
+    click_on 'Create Profile'
+
+    expect(page).to have_content('Bio Max Length is 500 characters')
+    expect(current_path).to eq new_profile_path 
+  end
+
 
   scenario 'And has a default picture if none is used' do
+    user = create(:user)
+    login_as(user)
 
+    visit root_path
+
+    fill_in 'Nickname:', with: 'Test123'
+    fill_in 'Bio', with: "A" * 499
+    click_on 'Create Profile'
+
+    expect(Profile.last.avatar.attached?).to eq true
   end
+
 end
 
