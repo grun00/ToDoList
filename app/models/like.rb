4f3ee94dca6a585cd3b155931f2345da33584413
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-18 15:58:39 -0300

    Removed Like model, WIP The Like System

--- app/models/like.rb
+++
@@ -1,6 +0,0 @@
-class Like < ApplicationRecord
-  enum current: {neither: 0, plus: 10, minus: 20}
-
-  belongs_to :user
-  belongs_to :comment
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   has_one :profile , dependent: :destroy 
   has_many :tasks
   has_many :comments, dependent: :destroy 
-  has_many :likes, dependent: :destroy 
 end
